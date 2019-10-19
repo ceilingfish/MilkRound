5f2f9b89ef424b502dec634ac48cd97b5c61488e
Tom Parrish <tom.parrish@crispthinking.com>
2019-10-19 22:16:53 +0100

    add login view

--- src/Ceilingfish.MilkRound.App/Ceilingfish.MilkRound.App/ViewModels/LoginViewModel.cs
+++ src/Ceilingfish.MilkRound.App/Ceilingfish.MilkRound.App/ViewModels/LoginViewModel.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Ceilingfish.MilkRound.Mobile.Core.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string username = "me", password = nameof(password);
+
+        public string Username
+        {
+            set
+            {
+                if (username != value)
+                {
+                    username = value;
+
+                    OnPropertyChanged();
+                }
+            }
+            get => username;
+        }
+
+        public string Password
+        {
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+
+                    OnPropertyChanged();
+                }
+            }
+            get => password;
+        }
+
+        public bool IsEnabled
+        {
+            get
+            {
+                s
+            }
+        }
     }
 }
--- src/Ceilingfish.MilkRound.App/Ceilingfish.MilkRound.App/Views/Login.xaml.cs
+++ src/Ceilingfish.MilkRound.App/Ceilingfish.MilkRound.App/Views/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Ceilingfish.MilkRound.Mobile.Core.ViewModels;
+﻿using Ceilingfish.MilkRound.Mobile.Core.Services;
+using Ceilingfish.MilkRound.Mobile.Core.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,10 +9,13 @@ namespace Ceilingfish.MilkRound.Mobile.Core
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        private readonly IAccountProvider accountProvider;
+
         public Login()
         {
+            this.accountProvider = DependencyService.Resolve<IAccountProvider>();
             InitializeComponent();
-            BindingContext = new LoginViewModel();
+            var loginState = new LoginViewModel();
         }
     }
 }
